4d7ea0a18de4bafcd97677cbfb5c3967a9c65595
steven-mi <stevenmi@live.de>
2020-10-14 00:52:16 +0200

    fix error in scripts

--- gpt2-service/setup.sh
+++ gpt2-service/setup.sh
@@ -1,8 +1,7 @@
-mkdir ./conf/trained_models
+mkdir -p ./conf/trained_models
 
 # download dummy model
 wget https://github.com/NewsPipe/gpt2-tfx-pipeline/releases/download/0.0.1/gpt355_dummy.zip
 unzip gpt355_dummy
-mkdir ./conf/trained_models
 mv ./gpt355_dummy ./conf/trained_models/
 rm gpt355_dummy.zip
