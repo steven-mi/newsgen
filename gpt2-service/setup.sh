fdb7d3b1c1f39ef4b4f66bbe5a4743e9bf631ddf
steven-mi <stevenmi@live.de>
2020-12-01 19:31:58 +0100

    add wiki model

--- gpt2-service/setup.sh
+++ gpt2-service/setup.sh
@@ -1,5 +1,6 @@
-# download dummy model
-wget https://github.com/NewsPipe/gpt2-tfx-pipeline/releases/download/0.0.1/gpt355_dummy.zip
-unzip gpt355_dummy
 mkdir -p ./gpt2service-1.0/conf/trained_models
-mv ./gpt355_dummy ./gpt2service-1.0/conf/trained_models/
+
+# download dummy model
+wget https://github.com/NewsPipe/gpt2-tfx-pipeline/releases/download/1.0/gpt355_german-wiki.zip
+unzip gpt355_german-wiki
+mv ./gpt355_german-wiki ./gpt2service-1.0/conf/trained_models/
