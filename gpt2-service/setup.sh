344e29d86e0cc27d5e17ecf9a7094c69f4bce50f
Steven <stevenmi@live.de>
2021-01-19 22:18:36 +0100

    Update setup.sh

--- gpt2-service/setup.sh
+++ gpt2-service/setup.sh
@@ -1,6 +1,5 @@
 mkdir -p ./gpt2service-1.0/conf/trained_models
 
-# download dummy model
-wget https://github.com/NewsPipe/gpt2-tfx-pipeline/releases/download/1.0/gpt355_german-wiki.zip
-unzip gpt355_german-wiki
-mv ./gpt355_german-wiki ./gpt2service-1.0/conf/trained_models/
+wget https://github.com/NewsPipe/gpt2-tfx-pipeline/releases/download/1.1/gpt355_german-news.zip
+unzip gpt355_german-news
+mv ./gpt355_german-news ./gpt2service-1.0/conf/trained_models/
