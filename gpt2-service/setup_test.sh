6a54b26c4783c845c5621ade57cc794f47f81aa3
Steven <stevenmi@live.de>
2021-01-27 13:26:26 +0100

    Update setup_test.sh

--- gpt2-service/setup_test.sh
+++ gpt2-service/setup_test.sh
@@ -1,4 +1,8 @@
 # for tests
 wget https://github.com/NewsPipe/gpt2-tfx-pipeline/releases/download/0.0.1/gpt355_dummy.zip
 unzip gpt355_dummy
-cp -r ./gpt355_dummy/* ./test/models/
+
+mkdir ./test/models/trained_models
+
+mv gpt355_dummy tfmodel
+cp -r tfmodel ./test/models/trained_models
